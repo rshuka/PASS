4a7c96ab505de6c7e1dc99b92cbada5bf0dc8e0d
Romeo Shuka <shuka@sra.uni-hannover.de>
2017-10-25 15:50:54 +0200

    try a apple fix

--- include/pass_bits/config.hpp
+++ include/pass_bits/config.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#pragma once
 
 // Armadillo
 #include <armadillo>
@@ -7,13 +8,15 @@
 // Therefore, CMake will decide whether SUPPORT_MPI is to be defined or not.
 /* #undef SUPPORT_MPI */
 
-// OpenMP support must be added via CMake, to ensure that we also link against it.
-// Therefore, CMake will decide whether SUPPORT_OPENMP is to be defined or not.
+// OpenMP support must be added via CMake, to ensure that we also link against
+// it. Therefore, CMake will decide whether SUPPORT_OPENMP is to be defined or
+// not.
 /* #undef SUPPORT_OPENMP */
 
 // The maximal number of threads to be supported by PASS.
 // Larger values may result in a greater start up time and decrese efficency.
-// In case `MAXIMAL_NUMBER_OF_THREADS` was not defined before, we fall back to the value below, determined via CMake.
+// In case `MAXIMAL_NUMBER_OF_THREADS` was not defined before, we fall back to
+// the value below, determined via CMake.
 #if !defined(MAXIMAL_NUMBER_OF_THREADS)
 
 #define MAXIMAL_NUMBER_OF_THREADS 1
@@ -21,8 +24,8 @@
 #endif
 
 namespace pass {
-  arma::uword threadNumber();
-  arma::uword numberOfThreads();
-  arma::uword nodeRank();
-  arma::uword numberOfNodes();
-}
+arma::uword threadNumber();
+arma::uword numberOfThreads();
+arma::uword nodeRank();
+arma::uword numberOfNodes();
+}  // namespace pass
