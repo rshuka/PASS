2e0807ec32882b1e7a815078bbd748763e4b85c3
Philipp Schiffmann <philippschiffmann93@gmail.com>
2018-03-15 17:59:11 +0100

    Add `evaluation_time_stall` class that artifically increases evaluation time of a problem.

---
+++ include/pass_bits/helper/evaluation_time_stall.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "pass_bits/problem.hpp"
+
+namespace pass
+{
+
+/**
+ * This helper class wraps another `problem` object and artifically increases
+ * the calculation time. Every time `evaluation_time_stall::evaluate()` is
+ * called, the call is forwarded to `wrapped_problem` and repeated
+ * `repetitions` times.
+ */
+class evaluation_time_stall : public problem
+{
+public:
+  /**
+   * The internal problem to which `evaluate` calls are forwarded.
+   *
+   * CAUTION: This variable is a reference type! Do not free or reuse the
+   * memory of `wrapped_object` until `evaluation_time_stall` is destroyed!
+   */
+  const pass::problem &wrapped_problem;
+
+  /**
+   * The number of times every `evaluate()` call is repeated. Must be 1 or
+   * greater.
+   */
+  arma::uword repetitions;
+
+  /**
+   * Initializes this object with the same bounds as `wrapped_problem` and
+   * `repetitions` set to 1.
+   */
+  evaluation_time_stall(const pass::problem &wrapped_problem);
+
+  virtual double evaluate(const arma::vec &agent) const override;
+};
+} // namespace pass
---
+++ src/helper/evaluation_time_stall.cpp
@@ -0,0 +1,18 @@
+#include "pass_bits/helper/evaluation_time_stall.hpp"
+
+pass::evaluation_time_stall::evaluation_time_stall(const pass::problem &wrapped_problem)
+    : problem(wrapped_problem.lower_bounds, wrapped_problem.upper_bounds),
+      wrapped_problem(wrapped_problem),
+      repetitions(1) {}
+
+double pass::evaluation_time_stall::evaluate(const arma::vec &agent) const
+{
+  assert(repetitions >= 1 && "`repetititions` must be at least 1");
+
+  double objective_value = 0.0;
+  for (arma::uword n = 0; n < repetitions; n++)
+  {
+    objective_value = wrapped_problem.evaluate(agent);
+  }
+  return objective_value;
+}
