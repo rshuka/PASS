7bec20c161aa3bc90ce6ca3a7d65387fb859389d
Romeo Shuka <shuka@sra.uni-hannover.de>
2018-10-25 16:56:39 +0200

    openMP test

--- include/pass_bits/analyser/openmp.hpp
+++ include/pass_bits/analyser/openmp.hpp
@@ -12,6 +12,6 @@ namespace pass
  * the same process will always be something new, so, it is not required to get the same result always!
  *
  */
-bool enable_openmp(const pass::problem &problem);
+bool enable_openmp(const pass::problem &problem, const int &training);
 
 } // namespace pass
--- src/analyser/openmp.cpp
+++ src/analyser/openmp.cpp
@@ -5,22 +5,33 @@
 #include "pass_bits/optimiser/parallel_swarm_search.hpp"
 #include "pass_bits/optimiser/particle_swarm_optimisation.hpp"
 #include "pass_bits/analyser/problem_evaluation_time.hpp"
+#include "pass_bits/helper/random.hpp"
 
-bool pass::enable_openmp(const pass::problem &problem)
+bool pass::enable_openmp(const pass::problem &problem, const int &training)
 {
   // set random seed
   arma::arma_rng::set_seed_random();
 
+  // min and max of training data
+  int min = 10;
+  int max = 2000;
+
   // define the maximum of runs
   arma::uword alg_runs = 2;
 
-  // Array including all alg runtime, we want to test
-  std::array<int, 20> repetitions = {10, 20, 40, 60, 70, 100, 120, 140, 160, 200, 240, 280, 300, 400, 500, 600, 700, 800, 900, 1000};
+  arma::rowvec data_normalised(training, arma::fill::randu);
+
+  arma::rowvec data(training);
+
+  for (int i = 0; i < training; i++)
+  {
+    data(i) = data_normalised(i) * max + min;
+  }
 
   arma::vec serial(alg_runs);
   arma::vec parallel(alg_runs);
 
-  arma::mat summary(repetitions.size(), 2);
+  arma::mat summary(data.size(), 2);
 
   pass::particle_swarm_optimisation algorithm_serial;
   algorithm_serial.maximal_duration = std::chrono::seconds(5);
@@ -30,7 +41,7 @@ bool pass::enable_openmp(const pass::problem &problem)
 
   int count = 0;
 
-  for (auto repetition : repetitions)
+  for (auto repetition : data)
   {
     // Problem initialisation
     pass::ackley_function problem(50);
