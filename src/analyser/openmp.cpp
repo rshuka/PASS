414bb8cb1b0dc7a538f071ca5249cc31412f6400
Romeo Shuka <shuka@sra.uni-hannover.de>
2018-10-25 18:42:16 +0200

    fixes

--- src/analyser/openmp.cpp
+++ src/analyser/openmp.cpp
@@ -18,18 +18,23 @@ bool pass::enable_openmp(const pass::problem &problem)
   std::array<int, 30> repetitions = {1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 14, 15, 17, 20, 25, 28, 30, 33, 36,
                                      40, 45, 50, 60, 70, 80, 100, 120, 140, 160};
 
-  double your_time = pass::problem_evaluation_time(problem);
-
   // Output information
   std::cout << " =========================== Start openMP Analyse ========================= " << std::endl;
   std::cout << "                                                                            " << std::endl;
-  std::cout << " Your Problem Evaluation Time is: : " << your_time * 1e-6 << " microseconds." << std::endl;
+  std::cout << " Your Problem:                " << problem.name << std::endl;
+  std::cout << " Dimension:                   " << problem.dimension() << std::endl;
+
+  std::cout << " ============================= Start Evaluation =========================== " << std::endl;
+
+  double your_time = pass::problem_evaluation_time(problem);
+
+  std::cout << " Evaluation time: " << your_time * 1e-6 << " microseconds." << std::endl;
+  std::cout << "                                                                            " << std::endl;
+  std::cout << " ============================= End Evaluation  ============================ " << std::endl;
   std::cout << "                                                                            " << std::endl;
 
   // Output information
   std::cout << " ============================= Start Trainining =========================== " << std::endl;
-  std::cout << "                                                                            " << std::endl;
-  std::cout << " Training should take about 10 minutes.                                     " << std::endl;
   std::cout << "                                                                            " << std::endl;
 
   arma::vec serial(alg_runs);
@@ -48,9 +53,6 @@ bool pass::enable_openmp(const pass::problem &problem)
 
   for (auto repetition : repetitions)
   {
-    double temp_count = 30.0 / count;
-
-    std::cout << " \r" << 100 / temp_count << "% completed." << std::flush;
     // Problem initialisation
     pass::gtoc1 test_problem;
     pass::evaluation_time_stall simulated_problem(test_problem);
@@ -74,14 +76,18 @@ bool pass::enable_openmp(const pass::problem &problem)
 
     summary(1, count) = arma::median(parallel) / arma::median(serial);
     count++;
+
+    // load bar
+    double temp_count = 30.0 / count;
+    std::cout << " \r " << 100 / temp_count << " % completed." << std::flush;
   }
 
   std::cout << std::endl
             << std::endl
-            << "Training completed successfully.\n"
+            << " Training completed successfully.\n"
             << std::flush;
-
-  std::cout << " ========================================================================== " << std::endl;
+  std::cout << "                                                                            " << std::endl;
+  std::cout << " ===========================  End Training  =============================== " << std::endl;
 
   // Generating a regression object
   pass::regression r;
@@ -93,7 +99,7 @@ bool pass::enable_openmp(const pass::problem &problem)
   // Output information
   std::cout << " =========================== Start Building Models ======================== " << std::endl;
   std::cout << "                                                                            " << std::endl;
-  std::cout << " Building linear model for the training data                                " << std::endl;
+  std::cout << " Building linear model for the training data.                               " << std::endl;
   std::cout << "                                                                            " << std::endl;
 
   // Generating the linear model
@@ -102,7 +108,7 @@ bool pass::enable_openmp(const pass::problem &problem)
 
   if (linear_model(2) >= 0.9)
   {
-    std::cout << " Linear Model is suitable                                                   " << std::endl;
+    std::cout << " Linear Model is suitable.                                                  " << std::endl;
     double predict_linear = r.predict_linear(your_time, linear_model);
 
     if (predict_linear > 0.9 * pass::number_of_threads())
@@ -110,9 +116,9 @@ bool pass::enable_openmp(const pass::problem &problem)
       predict_linear = 0.9 * pass::number_of_threads();
     }
     std::cout << "                                                                            " << std::endl;
-    std::cout << " Your speedUp would be approximately " << predict_linear << std::endl;
+    std::cout << " Your speedUp would be approximately: " << predict_linear << std::endl;
 
-    if (predict_linear > pass::number_of_threads() / 2) // is efficienty is more than 50 %
+    if (predict_linear > pass::number_of_threads() / 2) // efficienty is more than 50 %
     {
       std::cout << "                                                                            " << std::endl;
       std::cout << " You should activate openMP!                                                " << std::endl;
@@ -124,19 +130,18 @@ bool pass::enable_openmp(const pass::problem &problem)
       std::cout << " You should NOT activate openMP!                                            " << std::endl;
       return false;
     }
-    if (predict_linear > 1 && predict_linear < pass::number_of_threads() / 2) // is efficienty is more than 50 %
+    if (predict_linear > 1 && predict_linear < pass::number_of_threads() / 2) // efficienty is less than 50 %
     {
       std::cout << "                                                                            " << std::endl;
-      throw std::runtime_error(
-          " You should decide yourself if to activate openMP or not");
+      std::cout << " You should decide yourself if to activate openMP or not                    " << std::endl;
     }
   }
-  std::cout << " Linear Model is NOT suitable                                               " << std::endl;
+  std::cout << " Linear Model is NOT suitable.                                              " << std::endl;
   std::cout << "                                                                            " << std::endl;
-  std::cout << "  Finished building linear model                                            " << std::endl;
+  std::cout << " Finished building linear model                                            " << std::endl;
 
   std::cout << "                                                                            " << std::endl;
-  std::cout << " Building polynomial model for the training data                            " << std::endl;
+  std::cout << " Building polynomial model for the training data.                           " << std::endl;
   std::cout << "                                                                            " << std::endl;
 
   arma::rowvec poly_model = r.poly_model(x_values, y_values);
@@ -148,7 +153,7 @@ bool pass::enable_openmp(const pass::problem &problem)
     predict_poly = 0.9 * pass::number_of_threads();
   }
   std::cout << "                                                                            " << std::endl;
-  std::cout << " Your speedUp would be approximately " << predict_poly << std::endl;
+  std::cout << " Your speedUp would be approximately: " << predict_poly << std::endl;
 
   if (predict_poly > pass::number_of_threads() / 2) // is efficienty is more than 50 %
   {
@@ -165,13 +170,12 @@ bool pass::enable_openmp(const pass::problem &problem)
   if (predict_poly > 1 && predict_poly < pass::number_of_threads() / 2) // is efficienty is more than 50 %
   {
     std::cout << "                                                                            " << std::endl;
-    throw std::runtime_error(
-        " You should decide yourself if to activate openMP or not");
+    std::cout << " You should decide yourself if to activate openMP or not                    " << std::endl;
   }
 
   std::cout << " ========================= Done Building Models  ========================== " << std::endl;
   std::cout << "                                                                            " << std::endl;
-  std::cout << " ========================= Done openMP Analyse  =========================== " << std::endl;
+  std::cout << " =========================  Done openMP Analyse  ========================== " << std::endl;
 
   return true;
 }
